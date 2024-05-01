ea97d11f7ec53dfc11b7e155459db89827db879a
Nuno Araújo <nu-nos@hotmail.com>
2024-05-01 11:46:27 +0100

    Part3 - Example 1 V4

--- part3/example1/mongo.js
+++ part3/example1/mongo.js
@@ -1,27 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://nuno:${password}@cluster0.trg1fdl.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://nuno:${password}@cluster0.trg1fdl.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
   const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean,
-  });
+  })
 
-  const Note = mongoose.model("Note", noteSchema);
-
-  const note = new Note({
-    content: "HTML is x",
-    important: true,
-  });
+  const Note = mongoose.model('Note', noteSchema)
 
   /* note.save().then(result => {
     console.log('note saved!')
@@ -30,8 +25,8 @@ mongoose.connect(url).then(() => {
 
   Note.find({}).then((result) => {
     result.forEach((note) => {
-      console.log(note);
-    });
-    mongoose.connection.close();
-  });
-});
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+})
