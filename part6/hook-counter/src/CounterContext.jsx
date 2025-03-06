8100b438fc668d8bf8237d9b2fc6c6673787bd92
Nuno Araujo <nuno.araujo@mota-engil.com>
2025-03-06 12:37:49 +0000

    Part 6 - Example V2

--- part6/hook-counter/src/CounterContext.jsx
+++ part6/hook-counter/src/CounterContext.jsx
@@ -15,16 +15,6 @@ const counterReducer = (state, action) => {
 
 const CounterContext = createContext()
 
-export const useCounterValue = () => {
-    const counterAndDispatch = useContext(CounterContext)
-    return counterAndDispatch[0]
-}
-
-export const useCounterDispatch = () => {
-    const counterAndDispatch = useContext(CounterContext)
-    return counterAndDispatch[1]
-}
-
 export const CounterContextProvider = (props) => {
     const [counter, counterDispatch] = useReducer(counterReducer, 0)
 
@@ -33,6 +23,16 @@ export const CounterContextProvider = (props) => {
             {props.children}
         </CounterContext.Provider>
     )
+}
+
+export const useCounterValue = () => {
+    const counterAndDispatch = useContext(CounterContext)
+    return counterAndDispatch[0]
+}
+
+export const useCounterDispatch = () => {
+    const counterAndDispatch = useContext(CounterContext)
+    return counterAndDispatch[1]
 }
 
 export default CounterContext
